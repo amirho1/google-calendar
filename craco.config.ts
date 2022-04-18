3622a393f01443b6e7a4515fdab0e09ea27db8f0
amirho1 <amirhosseinsalighedar@gmail.com>
2022-04-18 18:21:08 +0430

    add base url with .env also add fake server

---
+++ craco.config.ts
@@ -0,0 +1,18 @@
+import webpack from "webpack";
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          process: require.resolve("process/browser"),
+        },
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+      }),
+    ],
+  },
+};
