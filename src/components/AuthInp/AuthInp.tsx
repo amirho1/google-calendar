2e743a2085037f723c0e095a97d1e8d192ea252e
amirho1 <amirhosseinsalighedar@gmail.com>
2022-05-24 22:49:56 +0430

    add error and onBlur to authInp also change the design

--- src/components/AuthInp/AuthInp.tsx
+++ src/components/AuthInp/AuthInp.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useCallback, useState } from "react";
 import styles from "./AuthInp.module.scss";
 
 interface AuthInpProps {
@@ -8,6 +8,8 @@ interface AuthInpProps {
   value: string;
   onChange: (val: string) => void;
   require?: boolean;
+  error?: boolean;
+  onBlur?: React.FocusEventHandler<HTMLInputElement>;
 }
 
 const AuthInp: FC<AuthInpProps> = ({
@@ -16,19 +18,41 @@ const AuthInp: FC<AuthInpProps> = ({
   label,
   value,
   onChange,
+  onBlur,
   require = false,
+  error,
 }) => {
+  const [focus, setFocus] = useState(false);
+
+  const onFocus = useCallback(() => {
+    setFocus(true);
+  }, []);
+
+  const onInputBlur = useCallback(
+    e => {
+      setFocus(false);
+      onBlur && onBlur(e);
+    },
+    [onBlur]
+  );
+
   return (
     <div className={styles.AuthInp} data-testid="AuthInp">
       <input
-        className={`${styles.input}`}
+        className={`${styles.input} ${error ? styles.errorBorder : ""}`}
         value={value}
         type={type}
         id={htmlFor}
         onChange={e => onChange(e.currentTarget.value)}
         required={require}
+        onFocus={onFocus}
+        onBlur={onInputBlur}
       />
-      <label className={styles.label} htmlFor={htmlFor}>
+      <label
+        className={`${styles.label} ${error ? styles.errorColor : ""} ${
+          focus || value ? styles.labelTop : ""
+        }`}
+        htmlFor={htmlFor}>
         {label}
       </label>
     </div>
